9c8b1695cba1c34c03fa2d14525cb680bddb111f
parthavi15 <parthavimd00@gmail.com>
2024-01-10 18:26:07 +0530

    Make changes to the UpdateQuality method

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -6,50 +6,53 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
+      case item.name
+      when 'Aged Brie'
+        update_aged_brie(item)
+      when 'Sulfuras, Hand of Ragnaros'
+      when 'Backstage passes to a TAFKAL80ETC concert'
+        update_backstage_passes(item)
+      when 'Conjured Mana Cake'
+        update_conjured_item(item)
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
+        update_normal_item(item)
       end
     end
+  end
+
+  private
+
+  def update_aged_brie(item)
+    item.sell_in -= 1
+    item.quality += 1 if item.quality < 50
+    item.quality += 1 if item.sell_in < 0 && item.quality < 50
+  end
+
+  def update_backstage_passes(item)
+    item.sell_in -= 1
+    if item.sell_in < 0
+      item.quality = 0
+    elsif item.sell_in <= 5
+      item.quality += 3
+    elsif item.sell_in <= 10
+      item.quality += 2
+    else
+      item.quality += 1
+    end
+    item.quality = 50 if item.quality > 50
+  end
+
+  def update_conjured_item(item)
+    item.sell_in -= 1
+    item.quality -= 2 if item.quality > 0
+    item.quality -= 2 if item.sell_in < 0 && item.quality > 0
+  end
+
+  def update_normal_item(item)
+    item.sell_in -= 1
+    item.quality -= 1 if item.quality > 0
+    item.quality -= 2 if item.sell_in < 0 && item.quality > 0
+    item.quality = 0 if item.quality.negative?
   end
 end
 
